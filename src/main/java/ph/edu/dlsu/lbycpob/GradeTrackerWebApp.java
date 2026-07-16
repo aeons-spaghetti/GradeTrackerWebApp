08344927e9c750b262d67ef00e44abd9276c5ea5
Ethan Arsua <ethan_arsua@dlsu.edu.ph>
2026-07-16 11:01:08 +0800

    Shortened main filename

--- src/main/java/ph/edu/dlsu/lbycpob/GradeTrackerWebApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/GradeTrackerWebApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GradeTrackerWebAppApplication {
+public class GradeTrackerWebApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(GradeTrackerWebAppApplication.class, args);
+        SpringApplication.run(GradeTrackerWebApp.class, args);
     }
 
 }
