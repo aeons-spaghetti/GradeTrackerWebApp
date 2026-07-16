fa4dedacff4c667d2da788103f31541216146748
Ethan Arsua <ethan_arsua@dlsu.edu.ph>
2026-07-16 12:14:04 +0800

    Importing libraries and classes for GradeService

--- src/main/java/ph/edu/dlsu/lbycpob/service/GradeService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/service/GradeService.java
@@ -1,4 +1,15 @@
 package ph.edu.dlsu.lbycpob.service;
 
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.dto.StudentFormDTO;
+import ph.edu.dlsu.lbycpob.model.Student;
+import ph.edu.dlsu.lbycpob.util.GradeCalculator;
+import ph.edu.dlsu.lbycpob.util.IDVerifier;
+
+@Service
 public class GradeService {
+
+    public Student buildStudent(StudentFormDTO dto){
+
+    }
 }
